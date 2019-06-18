e2d0f2a16e3c847632f7e5ea9223b7f2785ef9d6
FruitRaccoon <fruitraccoon@outlook.com>
2019-06-18 13:47:24 +1000

    Add date parser for non-delimited dates

--- src/parsers/dateParser.test.ts
+++ src/parsers/dateParser.test.ts
@@ -70,3 +70,85 @@ describe('parse tests for delimited date input', () => {
     expect(result.status).toBe(false);
   });
 });
+
+describe('parse tests for non-delimited date input', () => {
+  it('Returns date for single digit', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('5');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2019-01-05T00:00:00'));
+  });
+
+  it('Returns date for two digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('23');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2019-01-23T00:00:00'));
+  });
+
+  it('Returns date for two digits with leading zero', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('03');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2019-01-03T00:00:00'));
+  });
+
+  it('Fails to return a date for invalid two digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('32');
+    expect(result.status).toBe(false);
+  });
+
+  it('Fails to return a date for three digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('123');
+    expect(result.status).toBe(false);
+  });
+
+  it('Returns date for four digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('1221');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2019-12-21T00:00:00'));
+  });
+
+  it('Returns date for four digits using non-US locale', () => {
+    const p = buildDateParser(fakeCurrentDate, { localeOverride: 'fr' });
+    const result = p.parse('2112');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2019-12-21T00:00:00'));
+  });
+
+  it('Fails to return a date for five digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('21189');
+    expect(result.status).toBe(false);
+  });
+
+  it('Returns date for six digits with 2000 millennium year', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('122110');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2010-12-21T00:00:00'));
+  });
+
+  it('Returns date for six digits with 1900 millennium year', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('122189');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('1989-12-21T00:00:00'));
+  });
+
+  it('Fails to return a date for seven digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('1211989');
+    expect(result.status).toBe(false);
+  });
+
+  it('Returns date for eight digits', () => {
+    const p = buildDateParser(fakeCurrentDate);
+    const result = p.parse('12212010');
+    expect(result.status).toBe(true);
+    expect(result.status && result.value).toStrictEqual(new Date('2010-12-21T00:00:00'));
+  });
+});
--- src/parsers/dateParser.ts
+++ src/parsers/dateParser.ts
@@ -4,14 +4,6 @@ export interface IDateParserOptions {
   localeOverride?: string | string[];
 }
 
-type DatePart = 'year' | 'month' | 'day';
-
-interface IDatePartMap {
-  day: number;
-  month: number;
-  year?: number;
-}
-
 export function buildDateParser(getCurrentDate: () => Date, options?: IDateParserOptions) {
   const orderedDateParts = getOrderedShortDateParts(options && options.localeOverride);
 
@@ -19,6 +11,8 @@ export function buildDateParser(getCurrentDate: () => Date, options?: IDateParse
 
   const digits = P.regex(/\d+/).desc('Digits');
   const digitsTyped = digits.map(n => Number.parseInt(n, 10));
+  const twoDigits = P.regexp(/\d{2}/).desc('Two digits');
+  const fourDigits = P.regexp(/\d{4}/).desc('Four digits');
 
   const delimitedDateParser = P.seq(
     digitsTyped,
@@ -26,19 +20,104 @@ export function buildDateParser(getCurrentDate: () => Date, options?: IDateParse
     digitsTyped,
     dateSep.fallback<string[]>([]),
     digitsTyped.fallback(undefined)
-  )
-    .map(([p1, _s, p2, _s2, p3]) => {
-      // If there is no year entered, then ignore the year positioning
-      const parts = p3 === null ? orderedDateParts.filter(p => p !== 'year') : orderedDateParts;
-      const inputs = p3 === null ? [p1, p2] : [p1, p2, p3];
+  ).map(([p1, _s, p2, _s2, p3]) => {
+    return buildDateFromParts(getCurrentDate, orderedDateParts, [p1, p2, p3]);
+  });
 
-      const map = parts.reduce((acc, p, i) => ({ ...acc, [p]: inputs[i] }), {} as IDatePartMap);
+  const nonDelimitedDateParser = digits.chain(nStr => {
+    switch (nStr.length) {
+      case 1:
+      case 2:
+        return P.succeed(
+          buildDateFromParts(getCurrentDate, orderedDateParts, [
+            Number.parseInt(nStr, 10),
+            undefined,
+            undefined,
+          ])
+        );
 
-      return getDate(map.year || getCurrentDate().getFullYear(), map.month, map.day);
-    })
-    .chain(d => (d === null ? P.fail('Invalid') : P.succeed(d)));
+      case 4: {
+        const p = twoDigits
+          .map(n => Number.parseInt(n, 10))
+          .times(2)
+          .map(r =>
+            buildDateFromParts(getCurrentDate, orderedDateParts, r as [
+              number,
+              number,
+              number | undefined
+            ])
+          )
+          .parse(nStr);
+        return p.status ? P.succeed(p.value) : P.fail(`Invalid date '${nStr}'`);
+      }
 
-  return delimitedDateParser;
+      case 6: {
+        const p = twoDigits
+          .map(n => Number.parseInt(n, 10))
+          .times(3)
+          .map(r =>
+            buildDateFromParts(getCurrentDate, orderedDateParts, r as [number, number, number])
+          )
+          .parse(nStr);
+        return p.status ? P.succeed(p.value) : P.fail(`Invalid date '${nStr}'`);
+      }
+
+      case 8:
+        const p = P.seq(twoDigits, twoDigits, fourDigits)
+          .map(ns => ns.map(n => Number.parseInt(n, 10)))
+          .map(r =>
+            buildDateFromParts(getCurrentDate, orderedDateParts, r as [number, number, number])
+          )
+          .parse(nStr);
+        return p.status ? P.succeed(p.value) : P.fail(`Invalid date '${nStr}'`);
+
+      default:
+        return P.fail(`Invalid date '${nStr}'`);
+    }
+  });
+
+  return P.alt(delimitedDateParser, nonDelimitedDateParser).chain(d =>
+    d === null ? P.fail('Invalid') : P.succeed(d)
+  );
+}
+
+type DatePart = 'year' | 'month' | 'day';
+
+interface IDatePartMap {
+  day: number;
+  month?: number;
+  year?: number;
+}
+
+function buildDateFromParts(
+  getCurrentDate: () => Date,
+  orderedDateParts: [DatePart, DatePart, DatePart],
+  inputDateParts: [number, number | undefined, number | undefined]
+) {
+  const inputs = inputDateParts.filter(p => !!p);
+  const ordered =
+    inputs.length === 1
+      ? orderedDateParts.filter(p => p === 'day')
+      : inputs.length === 2
+      ? orderedDateParts.filter(p => p !== 'year')
+      : orderedDateParts;
+
+  const map = ordered.reduce((acc, p, i) => ({ ...acc, [p]: inputs[i] }), {} as IDatePartMap);
+
+  const currentDate = getCurrentDate();
+  const currentYear = currentDate.getFullYear();
+  return getDate(
+    convertTwoDigitYear(map.year || currentYear, currentYear),
+    map.month || currentDate.getMonth() + 1,
+    map.day
+  );
+}
+
+function convertTwoDigitYear(input: number, currentYear: number) {
+  if (input >= 100) {
+    return input;
+  }
+  return Math.abs(input + 2000 - currentYear) > 50 ? input + 1900 : input + 2000;
 }
 
 function getDate(year: number, month: number, day: number) {
